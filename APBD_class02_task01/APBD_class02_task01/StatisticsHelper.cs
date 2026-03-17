e58891cddba5846c93b96da756db9dcf4ede2fbc
domi kami <domisiaczek.ka@gmail.com>
2026-03-17 22:09:25 +0100

    changed comment in statistics helper

--- APBD_class02_task01/APBD_class02_task01/StatisticsHelper.cs
+++ APBD_class02_task01/APBD_class02_task01/StatisticsHelper.cs
@@ -4,7 +4,7 @@ namespace StatisticsHelper {
 	 public void CalculateMax(int[] values){
 	 //main changes
 	 }
-	 //random comment
+	 //random comment THIS IS AWESOME FEATURE-CONFLICT CHANGE
 	 public int CalculateMin(int[] values){
 		int result = values[0];
 		for(int i=0;i<values.size;i++){
